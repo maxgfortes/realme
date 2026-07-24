0b9af7b31d9aeaeb3fde3cf8ea050df4a497e023
Maxgfortes <164797536+maxgfortes@users.noreply.github.com>
2026-07-23 23:32:41 -0300

    Delete src directory

--- src/js/pages/feed/feed.js
+++
@@ -1,2 +0,0 @@
-import "/src/shared/nav-pic.js";
-import "/src/js/pages/feed/greeting.js";
--- src/js/pages/friends/friends.js
+++
@@ -1,3 +0,0 @@
-import "/src/shared/nav-pic.js";
-import "/src/js/pages/friends/filters-modal.js";
-import "/src/js/pages/friends/search.js";
